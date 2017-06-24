3e5fda2a834331b639145190d7ae77f54c96658b
小野寺 恵 <megumi@onodera-megumu-no-MacBook.local>
2017-06-24 16:55:05 +0900

    TableView構成に必要なメソッドまで

--- CoreToDo/ViewController.swift
+++ CoreToDo/ViewController.swift
@@ -11,10 +11,48 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var taskTableView: UITableView!
+    
+    // MARK: - Properties for table view
+    
+    var tasks:[Task] = []
+    var tasksToShow:[String:[String]] = ["ToDo":[], "Shopping":[], "Assignment":[]]
+    let taskCategories:[String] = ["ToDo", "Shopping", "Assignment"]
+    
+    // MARK: - Vire Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        taskTableView.dataSource = self
+        taskTableView.delegate = self
+    }
+    
+    // MARK: - Table View Data Source
+    
+    // taskCategories[]に格納されている文字列がTableViewのセクションになる
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return taskCategories.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return taskCategories[section]
+    }
+    
+    // tasksToShowにカテゴリー（tasksToShowのキーとなっている）ごとのnameが格納されている
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasksToShow[taskCategories[section]]!.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let sectionData = tasksToShow[taskCategories[indexPath.section]]
+        let cellData = sectionData?[indexPath.row]
+        
+        cell.textLabel?.text = "\(cellData!)"
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
