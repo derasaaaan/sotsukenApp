19ed313f029c1383e744eb6cbbcddfd52afabf28
小野寺 恵 <megumi@MegMacPro.local>
2017-09-25 14:30:23 +0900

    新機能はじめの一歩

---
+++ CoreToDo/TaskSubdivisionViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TaskSubdivisionViewController.swift
+//  CoreToDo
+//
+//  Created by 小野寺 恵 on 2017/09/25.
+//  Copyright © 2017年 derasan. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TaskSubdivisionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
