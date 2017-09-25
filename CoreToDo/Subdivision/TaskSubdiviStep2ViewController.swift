c1051d8a65e1508252bbdf24002e20d695784a28
小野寺 恵 <megumi@MegMacPro.local>
2017-09-25 18:15:26 +0900

    storyboardレイアウト

---
+++ CoreToDo/Subdivision/TaskSubdiviStep2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TaskSubdiviStep2ViewController.swift
+//  CoreToDo
+//
+//  Created by 小野寺 恵 on 2017/09/25.
+//  Copyright © 2017年 derasan. All rights reserved.
+//
+
+import UIKit
+
+class TaskSubdiviStep2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
