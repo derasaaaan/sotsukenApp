41d3663b64c7eca40fefa8c712598f7d21710133
小野寺 恵 <megumi@MegMacPro.local>
2017-10-07 14:21:33 +0900

    とりあえず動くようになった

---
+++ CoreToDo/Subdivision/SubdivAddTaskViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SubdivAddTaskViewController.swift
+//  CoreToDo
+//
+//  Created by 小野寺 恵 on 2017/10/07.
+//  Copyright © 2017年 derasan. All rights reserved.
+//
+
+import UIKit
+
+class SubdivAddTaskViewController: UIViewController,UITextFieldDelegate {
+
+    @IBOutlet weak var subdivTextField: UITextField!
+    
+    var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var task: SubdivTask?
+    
+    var taskCategory = "Subdiv"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let pickerToolBar = UIToolbar(frame: CGRect(x: 0, y: self.view.frame.size.height/6, width: self.view.frame.size.width, height: 40.0))
+        pickerToolBar.layer.position = CGPoint(x: self.view.frame.size.width/2, y: self.view.frame.size.height-20.0)
+        pickerToolBar.barStyle = .blackTranslucent
+        pickerToolBar.tintColor = UIColor.white
+        pickerToolBar.backgroundColor = UIColor.black
+        
+        // taskにも値が代入されていたら、textFieldとsegmentedControlにそれを表示
+    }
+
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CoreToDo/Subdivision/SubdivTableViewCell1.swift
@@ -0,0 +1,31 @@
+//
+//  SubdivTableViewCell1.swift
+//  CoreToDo
+//
+//  Created by 小野寺 恵 on 2017/10/06.
+//  Copyright © 2017年 derasan. All rights reserved.
+//
+
+import UIKit
+
+class SubdivTableViewCell1: UITableViewCell {
+
+    // MARK: - Properties
+    
+    static let reuseItentifier = "MainTaskCell"
+    
+    // MARK: -
+    // ここらへんにoutlet接続
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
